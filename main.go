221d3846b3ff58bf76c07be69be6b58842b51e01
Anfimov <uanavaro@gmail.com>
2021-10-16 15:53:43 +0300

    add the ability to render HTML file witch contains repository file charts

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/components"
+
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
 	"go.uber.org/zap"
 )
 
@@ -31,6 +37,42 @@ func main() {
 		log.With(zap.Error(err)).Fatal("failed to get changeCharts")
 	}
 
-	charts := chartsFromFileChanges(fileChanges)
-	fmt.Println(charts)
+	filesChart := chartsFromFileChanges(fileChanges)
+
+	renderCharts(filesChart)
+}
+
+func renderCharts(chs []Chart) {
+	page := components.NewPage()
+	for _, c := range chs {
+		line := charts.NewLine()
+		// set some global options like Title/Legend/ToolTip or anything else
+		line.SetGlobalOptions(
+			charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
+			charts.WithTitleOpts(opts.Title{
+				Title: fmt.Sprintf("File: %s", c.Filename),
+			}))
+
+		var (
+			xAxis     []string
+			lineItems []opts.LineData
+		)
+		for _, point := range c.TimelinePoints {
+			xAxis = append(xAxis, point.Key)
+			lineItems = append(lineItems, opts.LineData{
+				Value: point.AmountOfChanges,
+			})
+		}
+
+		// Put data into instance
+		line.SetXAxis(xAxis).
+			AddSeries("changes", lineItems).
+			SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
+		page.AddCharts(line)
+	}
+
+	// Where the magic happens
+	f, _ := os.Create("charts.html")
+	_ = page.Render(f)
+	_ = f.Close()
 }
